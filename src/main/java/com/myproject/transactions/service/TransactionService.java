564995347b5c4e83d08a68020cab9f37ae04baf0
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-18 19:24:48 -0300

    refactoring validateTransaction and creating "doTransaction" method

--- src/main/java/com/myproject/transactions/service/TransactionService.java
+++ src/main/java/com/myproject/transactions/service/TransactionService.java
@@ -3,6 +3,7 @@ package com.myproject.transactions.service;
 import com.myproject.transactions.dto.TransactionDTO;
 import com.myproject.transactions.entity.TransactionEntity;
 import com.myproject.transactions.entity.UserEntity;
+import com.myproject.transactions.entity.UserTransactionType;
 import com.myproject.transactions.entity.UserType;
 import com.myproject.transactions.exception.InsufficientBalance;
 import com.myproject.transactions.repository.TransactionRepository;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 
 import java.math.BigDecimal;
+import java.util.IllegalFormatCodePointException;
 import java.util.List;
 
 @Service
@@ -28,22 +30,26 @@ public class TransactionService {
         return transactionRepository.findAll();
     }
 
-    public TransactionEntity createTransaction(TransactionDTO transaction) throws Exception {
-        UserEntity sender = userService.getUserById(transaction.getSenderId());
-        UserEntity receiver = userService.getUserById(transaction.getReceiverId());
+    public TransactionEntity createTransaction(TransactionDTO request) throws Exception {
+        UserEntity sender = userService.getUserById(request.getSenderId(), UserTransactionType.SENDER);
+        UserEntity receiver = userService.getUserById(request.getReceiverId(), UserTransactionType.RECEIVER);
 
         // validate the transaction
-        validateTransaction(sender, receiver, transaction.getAmount());
+        validateTransaction(sender, receiver, request.getAmount());
 
-        sender.setBalance(sender.getBalance().subtract(transaction.getAmount()));
-        receiver.setBalance(receiver.getBalance().add(transaction.getAmount()));
-
-        return transactionRepository.save(new TransactionEntity(sender, receiver, transaction.getAmount()));
+        return transactionRepository.save(new TransactionEntity(sender, receiver, request.getAmount()));
     }
 
     private void validateTransaction(UserEntity sender, UserEntity receiver, BigDecimal amount) {
         if (sender.getUserType().equals(UserType.SELLER)) throw new UserTypeInvalidException();
         if (sender.getBalance().compareTo(amount) < 0) throw new InsufficientBalance();
+
+        doTransaction(sender, receiver, amount);
+    }
+
+    private void doTransaction(UserEntity sender, UserEntity receiver, BigDecimal amount) {
+        sender.setBalance(sender.getBalance().subtract(amount));
+        receiver.setBalance(receiver.getBalance().add(amount));
     }
 
 }
