46551999d34e7b1864e8ab93432df8ae54e3b46b
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-17 22:59:43 -0300

    implementing endpoint findUserById

--- src/main/java/com/myproject/transactions/controller/UserController.java
+++ src/main/java/com/myproject/transactions/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserEntity>> listAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> findUserById(@PathVariable("id") Long id) throws Exception {
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.FOUND);
     }
 }
