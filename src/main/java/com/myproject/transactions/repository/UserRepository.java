4407943412779eb984561ffa5cac315a16a7e99f
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-18 01:35:02 -0300

    implementing find by document method

--- src/main/java/com/myproject/transactions/repository/UserRepository.java
+++ src/main/java/com/myproject/transactions/repository/UserRepository.java
@@ -7,4 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findUserById(Long id);
+    Optional<UserEntity> findUserByDocument(String document);
+    boolean existsByDocument(String document);
+    boolean existsByEmail(String email);
 }
