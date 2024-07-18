074095525a33080aa3a4859c8dd26dfae0aef0f8
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-17 22:47:53 -0300

    implementing user controller

---
+++ src/main/java/com/myproject/transactions/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.myproject.transactions.controller;
+
+import com.myproject.transactions.entity.UserEntity;
+import com.myproject.transactions.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserEntity>> listAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+}
