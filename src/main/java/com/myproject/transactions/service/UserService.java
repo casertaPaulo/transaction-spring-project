50c5f790e3ef58cca0ec7f1c004f79b9c8f579db
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-17 22:49:31 -0300

    implementing user service

---
+++ src/main/java/com/myproject/transactions/service/UserService.java
@@ -0,0 +1,20 @@
+package com.myproject.transactions.service;
+
+import com.myproject.transactions.entity.UserEntity;
+import com.myproject.transactions.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
