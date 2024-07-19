4119aa6ee8f9c5951407b809b8729039adb8827c
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-19 17:18:53 -0300

    creating find by id method

--- src/main/java/com/myproject/transactions/repository/AnnouncementRepository.java
+++ src/main/java/com/myproject/transactions/repository/AnnouncementRepository.java
@@ -3,5 +3,8 @@ package com.myproject.transactions.repository;
 import com.myproject.transactions.entity.AnnouncementEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AnnouncementRepository extends JpaRepository<AnnouncementEntity, String> {
+    Optional<AnnouncementEntity> findAnnouncementById(String id);
 }
