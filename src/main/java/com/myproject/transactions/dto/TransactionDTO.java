2b932864af229f1ed4d14ff119e3ba5e0e53a09b
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-19 17:21:07 -0300

    adding setters and creating specified constructor

--- src/main/java/com/myproject/transactions/dto/TransactionDTO.java
+++ src/main/java/com/myproject/transactions/dto/TransactionDTO.java
@@ -1,13 +1,25 @@
 package com.myproject.transactions.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class TransactionDTO {
 
     private Long senderId;
     private Long receiverId;
     private BigDecimal amount;
+
+
+    public TransactionDTO(Long senderId, Long receiverId, BigDecimal amount) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.amount = amount;
+    }
 }
