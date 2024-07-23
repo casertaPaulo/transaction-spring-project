efc053cd4f6f3e1ec6cf0810ad06886d38ea2bfb
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-22 23:11:22 -0300

    fix issue - removing getter annotation from record

--- src/main/java/com/myproject/transactions/dto/RequestAnnouncementDTO.java
+++ src/main/java/com/myproject/transactions/dto/RequestAnnouncementDTO.java
@@ -2,7 +2,6 @@ package com.myproject.transactions.dto;
 
 import lombok.Getter;
 
-@Getter
 public record RequestAnnouncementDTO(
         Long sellerId,
         Long productId
--- src/main/java/com/myproject/transactions/dto/RequestOrderDTO.java
+++ src/main/java/com/myproject/transactions/dto/RequestOrderDTO.java
@@ -2,7 +2,7 @@ package com.myproject.transactions.dto;
 
 import lombok.Getter;
 
-@Getter
+
 public record RequestOrderDTO (
         String announcementId,
         Long buyerId
