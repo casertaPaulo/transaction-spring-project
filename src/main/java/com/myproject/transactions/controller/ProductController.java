5db3542b894403d29310a0d9e1e3e5da4395ecc4
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-22 21:33:14 -0300

    implementing feature find by id

--- src/main/java/com/myproject/transactions/controller/ProductController.java
+++ src/main/java/com/myproject/transactions/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.myproject.transactions.controller;
 
 import com.myproject.transactions.entity.ProductEntity;
 import com.myproject.transactions.service.ProductService;
+import org.aspectj.apache.bcel.generic.RET;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,22 +19,26 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<List<ProductEntity>> listAllProducts() {
-        return new ResponseEntity<>(productService.listAllProducts(), HttpStatus.OK);
+        return ResponseEntity.ok(productService.listAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductEntity> findProductById(@PathVariable("id") Long id ) {
+        return ResponseEntity.ok(productService.findProductById(id));
     }
 
     @PostMapping
     public ResponseEntity<ProductEntity> createProduct(@RequestBody ProductEntity product) {
-        return new ResponseEntity<>(productService.createProduct(product), HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(product));
     }
 
     @PutMapping
     public ResponseEntity<ProductEntity> updateProduct(@RequestBody ProductEntity product) {
-        return new ResponseEntity<>(productService.updateProduct(product), HttpStatus.OK);
+        return ResponseEntity.ok(productService.updateProduct(product));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ProductEntity> deleteProduct(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(productService.deleteProduct(id), HttpStatus.OK);
+        return ResponseEntity.ok(productService.deleteProduct(id));
     }
-
 }
